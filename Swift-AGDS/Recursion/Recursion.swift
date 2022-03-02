ba9e3bbb02f7d7c30e84b3dac2b282cdda45036b
Derrick Park <derrick8689@gmail.com>
2022-03-02 14:48:17 -0800

    Add assignmen1 comment

--- Swift-AGDS/Recursion/Recursion.swift
+++ Swift-AGDS/Recursion/Recursion.swift
@@ -69,3 +69,16 @@ func reverseLines(_ line: Int = 0) {
     print(lines[line])
   }
 }
+
+/// 5. evaluate
+/// Write a recursive function evaluate that accepts a string representing a math expression and computes its value.
+/// - The expression will be "fully parenthesized" and will consist of + and * on single-digit integers only.
+/// - You can use a helper function. (Do not change the original function header)
+/// - Use Recursion
+/// evaluate("7")                 -> 7
+/// evaluate("(2+2)")             -> 4
+/// evaluate("(1+(2*4))")         -> 9
+/// evaluate("((1+3)+((1+2)*5))") -> 19
+func evaluate(_ expr: String) -> Int {
+  return 0
+}
